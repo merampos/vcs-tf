9dd88dc312acb6382267b40dfc0f087ac95a1112
Rampos-singh <136924418+Rampos-singh@users.noreply.github.com>
2023-12-25 21:49:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,9 @@
 resource "aws_vpc" "main" {
   cidr_block = "192.168.1.0/24"
 }
+resource "aws_vpc" "main2" {
+  cidr_block = "192.168.2.0/24"
+}
 output "name" {
   value = aws_vpc.main.cidr_block
 } 
